002685b863b34db566860e45fe3b4d7af7374a74
Helen <mart.velema@student.nhlstenden.com>
2024-06-09 01:12:04 +0200

    Added functional footer

--- webpage/components/footer.php
+++ webpage/components/footer.php
@@ -1,1 +1,33 @@
-<footer></footer>
+<footer>
+    <div>
+        <p>
+            This is the official website of the Infinite Night Timeline. <br><br>
+            Here you can read up on all the lore that is related to everything Infinite Night
+        </p>
+    </div>
+    <div>
+        <ul>
+            <li><a href="https://story.infinite-night.com/doc.php?d=hellbound-steel">Hellbound Steel</a></li>
+            <li><a href="https://story.infinite-night.com/doc.php?d=infinite-night">Mainline</a></li>
+            <li><a href="https://story.infinite-night.com/doc.php?d=tale-of-anita">Tale of Anita</a></li>
+            <li><a href="https://story.infinite-night.com/doc.php?d=violent-stars">Violent Stars</a></li>
+        </ul>
+    </div>
+    <div>
+        <p>
+            Where to find me (I swear I'll do something with my socials one day):
+        </p>
+        <ul>
+            <li><a href="https://discord.gg/xvNg7Br"><img src="img/faviconD.ico" alt="Discord" width="32" height="32"></a></li>
+            <li><a href="https://www.youtube.com/channel/UCPmDtSr9w7x0nsee1GsdC4A"><img src="img/faviconYT.ico" alt="YouTube" width="32" height="32"></a></li>
+            <li><a href="https://www.twitch.tv/1_hele_euro"><img src="img/faviconT.ico" alt="Twitch" width="32" height="32"></a></li>
+            <li><a href="https://twitter.com/1HeleEuro"><img src="img/faviconX.ico" alt="Twitter, AKA X" width="32" height="32"></a></li>
+        </ul>
+        <hr>
+        <p>
+            <?php
+                echo "&copy; 1970 - " . date('Y') . "  1_hele_euro"
+            ?>
+        </p>
+    </div>
+</footer>
