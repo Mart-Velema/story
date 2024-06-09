8c569dee074a31b38f07feaf1f7deff860f7647f
Helen <mart.velema@student.nhlstenden.com>
2024-06-09 13:29:58 +0200

    Added JSON decoder

--- webpage/index.php
+++ webpage/index.php
@@ -47,9 +47,30 @@
                     </form>
                 </div>
             </div>
+            <hr>
             <!-- Auto-generated blocks -->
             <?php
+                $jsonFile = 'documents.json';
+                $jsonData = file_exists($jsonFile) ? json_decode(file_get_contents($jsonFile), true) : [];
+                if (json_last_error() !== JSON_ERROR_NONE) 
+                {
+                    echo("JSON decode error: " . json_last_error_msg());
+                    $jsonData = [];
+                }
 
+                foreach($jsonData as $value)
+                {
+                    if (isset($value['link'], $value['title'], $value['body']))
+                    {
+                        echo 
+                        '<div class="block">' .
+                            '<a href="' . $value['link'] . '"><h2>' . $value['title'] . '</h2></a>' .
+                            '<div class="inner-block">' .
+                                '<p>' . $value['body'] . '</p>' .
+                            '</div>' .
+                        '</div>';
+                    }
+                }
             ?>
         </div>
     </main>
