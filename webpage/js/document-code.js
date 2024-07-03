4cfcdcc7c7e371f9b9c2fb73d2918ed0783dc258
Helen <mart.velema@student.nhlstenden.com>
2024-07-04 00:11:27 +0200

    Fixed copy-code button

--- webpage/js/document-code.js
+++ webpage/js/document-code.js
@@ -1,6 +1,7 @@
 function copyCodeToClipboard()
 {
     let documentCode = document.getElementById('document-code');
-    navigator.clipboard.writeText(documentCode.value);
+    let text = documentCode.textContent || documentCode.innerText;
+    navigator.clipboard.writeText(text);
     alert("Copied " + documentCode.textContent + " to the clipboard");
 }
