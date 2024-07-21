548920c8268149574a8ea1c18c9d21e92519724d
Helen <mart.velema@student.nhlstenden.com>
2024-07-21 02:22:01 +0200

    Added Google verification

--- webpage/index.php
+++ webpage/index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css?v=<?php echo filemtime('css/style.css'); ?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <meta name="google-site-verification" content="CptONgedSFzgLS7YtrEG8C7zTi0u1byKmekdc1v1Qo0" />
     <title>Infinite Night docs</title>
 </head>
 <body>
