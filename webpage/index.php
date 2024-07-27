056c33935ecf50cfb5195543b55f9dbd5ba13815
Helen <mart.velema@student.nhlstenden.com>
2024-07-28 01:50:34 +0200

    Improved performance of CSS loading

--- webpage/index.php
+++ webpage/index.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css?v=<?php echo filemtime('css/style.css'); ?>">
+    <link rel="stylesheet" href="css/header.css?v=<?php echo filemtime('css/header.css'); ?>">
+    <link rel="stylesheet" href="css/footer.css?v=<?php echo filemtime('css/footer.css'); ?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <meta name="google-site-verification" content="CptONgedSFzgLS7YtrEG8C7zTi0u1byKmekdc1v1Qo0" />
-    <title>Infinite Night docs</title>
+    <title>Infinite Night | docs</title>
 </head>
 <body>
     <?php
