e5b0a4c4d806a68144a2e5da7f7ced58d7684495
Helen <mart.velema@student.nhlstenden.com>
2024-07-29 03:20:53 +0200

    Added option to use Python based updater

--- config_example.sh
+++ config_example.sh
@@ -2,6 +2,7 @@ basedir= # Directory where the document directories are loacted
 outputdir= # Directory where the HTML files will be saved
 directories= # List of directories where the documents are stored
 resetGit=0
+usePythonUpdater=0
 
 # Example:
 # basedir='/mnt/nas/docs'
--- update.sh
+++ update.sh
@@ -13,4 +13,14 @@ else
     echo "Not updating Git..."
 fi
 
-./extractBodyBulk.sh
+if [ $usePythonUpdater -eq 1];
+then
+    if command -v -- python3 > /dev/null 2>&1;
+    then
+        python3 extractBodyBulk.py
+    else
+        echo "Python is not installed! Please install Python with 'sudo apt install python3', or your package manager of choice"
+    fi
+else
+    ./extractBodyBulk.sh
+fi
