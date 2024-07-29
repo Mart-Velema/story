5d1c78d69fda60693ecc3799eab2ce25ab74c86c
Helen <mart.velema@student.nhlstenden.com>
2024-07-29 13:24:52 +0200

    Re-added depricated versions

---
+++ depricated/config_example.sh
@@ -0,0 +1,9 @@
+basedir= # Directory where the document directories are loacted
+outputdir= # Directory where the HTML files will be saved
+directories= # List of directories where the documents are stored
+resetGit=0
+
+# Example:
+# basedir='/mnt/nas/docs'
+# outputdir='/var/www/docs'
+# directories=('directory1' 'directory2' 'directory3')
