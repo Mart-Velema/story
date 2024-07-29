47d5b114998051199b44516c53f1a02c15518d15
Helen <mart.velema@student.nhlstenden.com>
2024-07-29 13:22:13 +0200

    Depricted bash based document converter

--- update.sh
+++ update.sh
@@ -2,25 +2,15 @@
 
 cd /var/www/story
 
-source config.sh
 date -d @$(date -u +%s)
-if [ $resetGit -eq 1 ];
-then
-    echo "Updating Git..."
-    git restore .
-    git pull
-else
-    echo "Not updating Git..."
-fi
 
-if [ $usePythonUpdater -eq 1];
+echo "Updating Git..."
+git restore .
+git pull
+
+if command -v -- python3 > /dev/null 2>&1;
 then
-    if command -v -- python3 > /dev/null 2>&1;
-    then
-        python3 extractBodyBulk.py
-    else
-        echo "Python is not installed! Please install Python with 'sudo apt install python3', or your package manager of choice"
-    fi
+    python3 extractBodyBulk.py
 else
-    ./extractBodyBulk.sh
+    echo "Python is not installed! Please install Python with 'sudo apt install python3', or your package manager of choice"
 fi
