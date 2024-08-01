58d44489eeb3adc7bacf8f9b81cdd386e8ed1d00
Helen <mart.velema@student.nhlstenden.com>
2024-08-02 00:31:53 +0200

    Improved error handling, updated string manipulation to use os functions instead

--- extractBodyBulk.py
+++ extractBodyBulk.py
@@ -3,32 +3,41 @@ import os
 from config import *
 
 def convert_odt_to_html(sub_directory, filename):
-    final_file_name = \
-        (sub_directory + '-|-' + filename) \
-        .replace(' ', '-') \
-        .lower() \
-        .strip('.odt')
-    print('Converting file: ' + input_directory + '/' + sub_directory + '/' + filename)
-    pandoc.convert_file(
-        source_file = input_directory + '/' + sub_directory + '/' + filename,
-        to = 'html5',
-        outputfile = output_directory + '/' + final_file_name + '.html',
-        extra_args = [
-            '--toc',
-            '--extract-media=' + output_directory + '/assets',
-            '--template=html.template',
-            '--lua-filter=filter.lua',
-            '--self-contained',
-            '--quiet'
-        ]
-    )
-    print('Converted file to: ' + final_file_name)
+    base_name, file_extention = os.path.splitext(filename)
+    if file_extention.lower() != ".odt":
+        return
 
+    final_file_name = \
+        f"{sub_directory}-|-{base_name}.html" \
+        .replace(' ', '-') \
+        .lower()
+    
+    source_file = os.path.join(input_directory, sub_directory, filename)
+    output_file = os.path.join(output_directory, final_file_name)
+
+    try:
+        print(f'Converting file: {source_file}')
+        pandoc.convert_file(
+            source_file = source_file,
+            to = 'html5',
+            outputfile = output_file,
+            extra_args = [
+                '--toc',
+                f'--extract-media={os.path.join(output_directory, "assets")}',
+                '--template=html.template',
+                '--lua-filter=filter.lua',
+                '--self-contained',
+                '--quiet'
+            ]
+        )
+        print(f'Converted file to: {final_file_name}')
+    except Exception as ex:
+        print(f'Failed to convert file {final_file_name}: {ex}')
 
 # preparing directories
 os.makedirs(output_directory + '/assets', exist_ok=True)
 
 for sub_directory in sub_directories:
-    for file in os.listdir(input_directory + '/' + sub_directory):
-        if file[-4:] == '.odt':
-            convert_odt_to_html(sub_directory, file)
+    sub_dir_path = os.path.join(input_directory, sub_directory)
+    for file in os.listdir(sub_dir_path):
+        convert_odt_to_html(sub_directory, file)
