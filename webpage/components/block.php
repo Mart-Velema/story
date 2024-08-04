af6ddbca452b7317da6931b92716192ba2954867
Helen <mart.velema@student.nhlstenden.com>
2024-08-04 22:24:29 +0200

    re-added block.php

---
+++ webpage/components/block.php
@@ -0,0 +1,28 @@
+<?php   
+if(!isset($jsonFile))
+{
+    echo('JSON decode error: JSON filename is not declared');
+    $jsonData = [];
+}
+
+$jsonData = file_exists($jsonFile) ? json_decode(file_get_contents($jsonFile), true) : [];
+if (json_last_error() !== JSON_ERROR_NONE) 
+{
+    echo("JSON decode error: " . json_last_error_msg());
+    $jsonData = [];
+}
+
+foreach($jsonData as $value)
+{
+    if (isset($value['link'], $value['title'], $value['body']))
+    {
+        echo 
+        '<div class="block">' .
+            '<a href="/doc.php?d=' . $value['link'] . '"><h2>' . $value['title'] . '</h2></a>' .
+            '<div class="inner-block">' .
+                '<p>' . nl2br($value['body']) . '</p>' .
+            '</div>' .
+        '</div>';
+    }
+}
+?>
